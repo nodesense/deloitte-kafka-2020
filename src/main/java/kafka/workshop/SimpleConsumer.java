b4040e1c97df773cca5271eeb41440831855a2f5
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-01-20 17:16:09 +0530

    starter for all

---
+++ src/main/java/kafka/workshop/SimpleConsumer.java
@@ -0,0 +1,86 @@
+// SimpleConsumer.java
+package kafka.workshop;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+public class SimpleConsumer {
+    public static String BOOTSTRAP_SERVERS = "localhost:9092";
+    public static String TOPIC = "greetings";
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+
+        props.put(BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+
+        // Consumer group id, should be unique, partition allocated to consumers inside teh group
+        props.put(GROUP_ID_CONFIG, "greetings-consumer-group"); // offset, etc, TODO
+
+        // -- true, automatically commit the offset, automatically
+        props.put(ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(SESSION_TIMEOUT_MS_CONFIG, "30000");
+
+        // key deserialize the bytes data into String format, JSON,AVRO formats
+        props.put(KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        // value deserialize the bytes data into String format, JSON,AVRO formats
+
+        props.put(VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        // discussed later
+       //  props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+       // props.put(ConsumerConfig.CLIENT_ID_CONFIG, "your_client_id");
+       // props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // <Key as string, Value as string>
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        // subscribe from one or more topics
+        consumer.subscribe(Collections.singletonList(TOPIC));
+
+        System.out.println("Consumer starting..");
+
+
+        List<PartitionInfo> partitions = consumer.partitionsFor(TOPIC);
+        for (PartitionInfo partitionInfo: partitions) {
+            // partitionInfo.
+            System.out.println("Partition " + partitionInfo);
+        }
+
+        while (true) {
+            // Consumer poll for the data with wait time
+            // poll for msgs for 1 second, any messges within second, group together
+            // if no msg, exit in 1 second, records length is 0
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            if (records.count() == 0)
+                continue; // wait for more msg
+
+
+            // Iterate record and print the record
+            for (ConsumerRecord<String, String> record: records) {
+
+                System.out.printf("partition=%d, offset=%d\n", record.partition(),
+                        record.offset());
+
+                System.out.printf("key=%s,value=%s\n", record.key(), record.value());
+            }
+
+            Thread.sleep(2000);
+        }
+
+    }
+
+}
